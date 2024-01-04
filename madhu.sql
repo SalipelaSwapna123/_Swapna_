222c52517310de0173c20798fb8b0d45e19087b4
Salipela Swapna <130962712+SalipelaSwapna123@users.noreply.github.com>
2024-01-04 21:21:21 +0530

    Html programs

---
+++ madhu.sql
@@ -0,0 +1,15 @@
+use  madhu;
+create table friend4(
+name varchar(12), roll_number int );
+insert  into friend4 values("swapna",100);
+insert  into friend4 values("swathi",101);
+insert  into friend4 values("chitti",102);
+use swapna;
+drop table friend2;
+drop table friend3,friend;
+use madhu;
+create table friend2(
+name varchar(28),roll_num int);
+insert  into friend2 values("swapna",100);
+insert  into friend2 values("swathi",101);
+insert  into friend2 values("chitti",102);
